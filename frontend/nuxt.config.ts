718ea590ebd91144a28293d14664b4ffcaacaea8
dodolMerahJambu <harits.achmad2003@gmail.com>
2026-07-17 08:57:33 +0700

    add new faticon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Koperasi Operational Console',
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    },
+  },
   css: ['~/assets/css/main.css', '~/assets/css/improvements.css', '~/assets/css/payment.css'],
   runtimeConfig: {
     public: { apiBase: '/api' },
